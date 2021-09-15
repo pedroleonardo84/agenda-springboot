06d063229bf2d1d008e998cb07d31f24ab2b64f0
pedroleonardo84 <pedroleonardo.spfc@gmail.com>
2021-09-14 21:24:35 -0300

    Initial commit

---
+++ src/main/java/br/com/digitalhouse/agenda/AgendaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalhouse/agenda/AgendaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
