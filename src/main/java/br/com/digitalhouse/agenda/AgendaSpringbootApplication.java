a07f63161486a8ef7c76d64ef3f0c49b614c2b2b
pedroleonardo84 <pedroleonardo.spfc@gmail.com>
2021-09-30 20:04:02 -0300

    ajustes

--- src/main/java/br/com/digitalhouse/agenda/AgendaSpringbootApplication.java
+++ src/main/java/br/com/digitalhouse/agenda/AgendaSpringbootApplication.java
@@ -3,7 +3,10 @@ package br.com.digitalhouse.agenda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AgendaSpringbootApplication {
 
 	public static void main(String[] args) {
